5949cbd59b0d7bec9e5d42cffef84db38d6d340c
mayarhany <mayarhany604@gmail.com>
2025-02-04 12:26:52 +0200

    Add home js file

---
+++ js/home.js
@@ -0,0 +1,58 @@
+import {products} from "./products.js";
+
+const productList = document.getElementById('product-list');
+const badge = document.querySelector('.badge');
+
+let cart = JSON.parse(localStorage.getItem('cart')) || [];
+
+
+
+function displayProducts(){
+    productList.innerHTML = products.map(product =>
+        `<div class="col-lg-3 col-md-4 col-6">
+                <div class="card shadow">
+                    <img src="${product.image}" class="card-img-top" alt="Product 1" />
+                    <div class="card-body">
+                        <h5 class="card-title">${product.name}</h5>
+                        <p class="card-text">${product.description}</p>
+                        <div class="card-text d-flex align-items-center justify-content-between mb-3">
+                            <p class="mb-0">$${product.price}</p>
+                            <div class="stars">
+                                <span><i class="fa-solid fa-star"></i></span>
+                                <span><i class="fa-solid fa-star"></i></span>
+                                <span><i class="fa-solid fa-star"></i></span>
+                                <span><i class="fa-solid fa-star"></i></span>
+                            </div>
+                        </div>
+                        <a href="#" onclick="addToCart(${product.id})" class="main-btn d-inline-block w-100 text-center">Add to Cart</a>
+                    </div>
+                </div>
+            </div>`
+    ).join('');
+}
+
+
+function addToCart(id){
+    const product = products.find(product => product.id === id);
+    const existingItem = cart.find(item => item.id === id);
+    
+    if(existingItem){
+        existingItem.quantity++;
+    }
+    else{
+        cart.push({...product, quantity: 1});
+    }
+    
+    localStorage.setItem('cart', JSON.stringify(cart));
+    updateCartCount();
+    // console.log(cart);
+}
+window.addToCart = addToCart;
+
+function updateCartCount(){
+    badge.textContent = cart.reduce((total, item) => total + item.quantity, 0);
+}
+window.updateCartCount = updateCartCount;
+
+displayProducts();
+updateCartCount();
---
+++ js/products.js
@@ -0,0 +1,74 @@
+export const products = [
+    {
+    id: 1,
+    name: 'Product 1',
+    price: 100,
+    quantity: 5,
+    description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et, alias!',
+    category: 'Category 1',
+    image: './images/alex-lvrs-mTw_GePuRUE-unsplash.jpg'
+    },
+    {
+    id: 2,
+    name: 'Product 2',
+    price: 100,
+    quantity: 5,
+    description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et, alias!',
+    category: 'Category 1',
+    image: './images/alex-lvrs-mTw_GePuRUE-unsplash.jpg'
+    },
+    {
+    id: 3,
+    name: 'Product 3',
+    price: 100,
+    quantity: 5,
+    description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et, alias!',
+    category: 'Category 1',
+    image: './images/alex-lvrs-mTw_GePuRUE-unsplash.jpg'
+    },
+    {
+    id: 4,
+    name: 'Product 4',
+    price: 100,
+    quantity: 5,
+    description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et, alias!',
+    category: 'Category 1',
+    image: './images/alex-lvrs-mTw_GePuRUE-unsplash.jpg'
+    },
+    {
+    id: 5,
+    name: 'Product 5',
+    price: 100,
+    quantity: 5,
+    description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et, alias!',
+    category: 'Category 1',
+    image: './images/alex-lvrs-mTw_GePuRUE-unsplash.jpg'
+    },
+    {
+    id: 6,
+    name: 'Product 6',
+    price: 100,
+    quantity: 5,
+    description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et, alias!',
+    category: 'Category 1',
+    image: './images/alex-lvrs-mTw_GePuRUE-unsplash.jpg'
+    },
+    {
+    id: 7,
+    name: 'Product 7',
+    price: 100,
+    quantity: 5,
+    description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et, alias!',
+    category: 'Category 1',
+    image: './images/alex-lvrs-mTw_GePuRUE-unsplash.jpg'
+    },
+    {
+    id: 8,
+    name: 'Product 8',
+    price: 100,
+    quantity: 5,
+    description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et, alias!',
+    category: 'Category 1',
+    image: './images/alex-lvrs-mTw_GePuRUE-unsplash.jpg'
+    },
+]
